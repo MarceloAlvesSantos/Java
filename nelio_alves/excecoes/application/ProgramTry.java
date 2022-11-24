0819828f6089b98322e67fcce91a4756ce6f258a
Marcelo Alves Santos <marcelonecessario@hotmail.com>
2022-11-24 12:48:03 -0300

    Commit Tratamento de exceções

---
+++ nelio_alves/excecoes/application/ProgramTry.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ProgramTry {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		method1();
+		
+		
+		sc.close();
+	}
+	
+	public static void method1() {
+		System.out.println("***METHOD1 START***");
+		
+		method2();
+		
+		System.out.println("***METHOD1 END***");
+	}
+	
+	public static void method2() {
+		System.out.println("***METHOD2 START***");
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			 
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+		} 
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid position!" + e.getMessage());
+			e.printStackTrace();
+			sc.next();
+		} 
+		catch (InputMismatchException e) {
+			System.out.println("Input error!!");
+		}
+		
+		System.out.println("***METHOD2 END***");
+
+		
+		sc.close();
+	}
+
+}
